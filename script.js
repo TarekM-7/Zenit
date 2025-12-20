b13e1f9e207b997de63776d8d0f6e6911c29378d
Tarek Mariscal <tarekmrscl@gmail.com>
2025-12-20 10:16:27 -0600

    Add new function to see how many water of milk is left in the inventory, the values of milk and water in inventory are set in code right now, but I want to change it to user input

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-let coffeeGrams = 20;
-let americanoWater = 250;
-let machineWater = 60;
-let capuchinos;
-let total;
 
+let invWater = 10000;
+let invMilk = 10000;
 
 window.onload = function(){
     let coffee;
     let amount;
 
     document.getElementById("submit").onclick = function(){
-    coffee = document.getElementById("coffee-select").value;
-    amount = document.getElementById("amount").value;
-    console.log(coffee);
-    console.log(amount);
-    let coffeeResults = calculateTotalResources(coffee, amount);
+        coffee = document.getElementById("coffee-select").value;
+        amount = document.getElementById("amount").value;
+        console.log(coffee);
+        console.log(amount);
+        let coffeeResults = calculateTotalResources(coffee, amount);
 
-    document.getElementById("total-milk").textContent = coffeeResults.totalMilk;
-    document.getElementById("total-coffees").textContent = coffeeResults.numberOfCoffees;
-    document.getElementById("coffee-type").textContent = coffeeResults.coffeeType;
-    document.getElementById("total-coffee-grams").textContent = coffeeResults.totalCoffeeGrams;
-    document.getElementById("total-machine-water").textContent = coffeeResults.totalMachineWater;
-    document.getElementById("total-coffee-water").textContent = coffeeResults.water;
-    document.getElementById("total-water").textContent = coffeeResults.totalWater;
+        document.getElementById("total-milk").textContent = coffeeResults.totalMilk;
+        document.getElementById("total-coffees").textContent = coffeeResults.numberOfCoffees;
+        document.getElementById("coffee-type").textContent = coffeeResults.coffeeType;
+        document.getElementById("total-coffee-grams").textContent = coffeeResults.totalCoffeeGrams;
+        document.getElementById("total-machine-water").textContent = coffeeResults.totalMachineWater;
+        document.getElementById("total-coffee-water").textContent = coffeeResults.water;
+        document.getElementById("total-water").textContent = coffeeResults.totalWater;
 
-    console.log(`Total Milk is ${coffeeResults.totalMilk}ml for ${coffeeResults.numberOfCoffees} ${coffeeResults.coffeeType}s`)
-    console.log(`Total Coffee Grams is ${coffeeResults.totalCoffeeGrams}g`)
-    console.log(`Total Machine Water is ${coffeeResults.totalMachineWater}ml`)
-    console.log(`Total Coffee Water is ${coffeeResults.water}ml`)
-    console.log(`Total Water is ${coffeeResults.totalWater}ml`)
-    console.log("-----");
+        console.log(`Total Milk is ${coffeeResults.totalMilk}ml for ${coffeeResults.numberOfCoffees} ${coffeeResults.coffeeType}s`)
+        console.log(`Total Coffee Grams is ${coffeeResults.totalCoffeeGrams}g`)
+        console.log(`Total Machine Water is ${coffeeResults.totalMachineWater}ml`)
+        console.log(`Total Coffee Water is ${coffeeResults.water}ml`)
+        console.log(`Total Water is ${coffeeResults.totalWater}ml`)
+        console.log("-----");
+
+        let leftResources = calculateLeftResources(coffeeResults.totalWater, coffeeResults.totalMilk)
+
+        document.getElementById("water-left").textContent = leftResources.invWater;
+        document.getElementById("milk-left").textContent = leftResources.invMilk;
+
+        console.log(`${leftResources.invWater}ml of Water left.`);
+        console.log(`${leftResources.invMilk}ml of Milk left.`);
     }
 }
 
@@ -64,6 +69,17 @@ function calculateTotalResources(cup, numberOfCoffees){
         numberOfCoffees
     }
 
+}
+
+function calculateLeftResources(water, milk){
+
+    invWater -= water;
+    invMilk -= milk;
+
+    return{
+        invWater,
+        invMilk
+    }
 }
 
 /*
